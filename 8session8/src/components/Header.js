17c733fa313f88d0c91daf8ad13adce1c49c1f6a
Shanu <kumarsanu.2210@gmail.com>
2023-09-09 13:54:06 +0530

    8session8 folder added - Class Component

---
+++ 8session8/src/components/Header.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { HEADER_IMAGE } from "../utils/constants";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img src={HEADER_IMAGE} alt="" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+
+          <li>
+            <Link to={"/about"}>About Us</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact Us</Link>
+          </li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
